8f8a167daad2bf89f4c375fd874663e0c438e30b
Garrett Thornburg <film42@gmail.com>
2016-10-18 15:23:07 -0700

    Fix transaction model attributes to match latest docs

--- models/transaction.go
+++ models/transaction.go
@@ -1,34 +1,33 @@
 package models
 
 type Transaction struct {
-	TransactionTypeName  string  `json:"transaction_type_name,omitempty"`
 	AccountGuid          string  `json:"account_guid,omitempty"`
 	Amount               float64 `json:"amount,omitempty"`
+	Category             string  `json:"category,omitempty"`
+	CheckNumber          int64   `json:"check_number,omitempty"`
 	CreatedAt            string  `json:"created_at,omitempty"`
 	Description          string  `json:"description,omitempty"`
 	Guid                 string  `json:"guid,omitempty"`
-	HasBeenSplit         bool    `json:"has_been_split,omitempty"`
-	HasBeenViewed        bool    `json:"has_been_viewed,omitempty"`
 	IsBillPay            bool    `json:"is_bill_pay,omitempty"`
 	IsDirectDeposit      bool    `json:"is_direct_deposit,omitempty"`
 	IsExpense            bool    `json:"is_expense,omitempty"`
 	IsFee                bool    `json:"is_fee,omitempty"`
-	IsHidden             bool    `json:"is_hidden,omitempty"`
 	IsIncome             bool    `json:"is_income,omitempty"`
 	IsOverdraftFee       bool    `json:"is_overdraft_fee,omitempty"`
 	IsPayrollAdvance     bool    `json:"is_payroll_advance,omitempty"`
 	Latitude             float64 `json:"latitude,omitempty"`
 	Longitude            float64 `json:"longitude,omitempty"`
+	MemberGuid           string  `json:"member_guid,omitempty"`
 	Memo                 string  `json:"memo,omitempty"`
 	MerchantCategoryCode int64   `json:"merchant_category_code,omitempty"`
-	ParentGuid           string  `json:"parent_guid,omitempty"`
+	OriginalDescription  string  `json:"original_description,omitempty"`
 	PostedAt             string  `json:"posted_at,omitempty"`
-	TransactionType      int64   `json:"transaction_type,omitempty"`
+	Status               string  `json:"status,omitempty"`
+	TopLevelCategory     string  `json:"top_level_category,omitempty"`
 	TransactedAt         string  `json:"transacted_at,omitempty"`
+	Type                 string  `json:"type,omitempty"`
 	UpdatedAt            string  `json:"updated_at,omitempty"`
 	UserGuid             string  `json:"user_guid,omitempty"`
-	CheckNumber          int64   `json:"check_number,omitempty"`
-	FeedCheckNumber      int64   `json:"feed_check_number,omitempty"`
 }
 
 type TransactionResponse struct {
