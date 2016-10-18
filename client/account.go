d43115988404784e5be3a53b07df0892fc70272b
Garrett Thornburg <film42@gmail.com>
2016-10-18 07:13:24 -0700

    Add account and account-transaction requests + stub out transaction

--- client/account.go
+++ client/account.go
@@ -2,16 +2,89 @@ package client
 
 import (
 	"github.com/mxenabled/atrium-go/models"
+	"bytes"
+	"encoding/json"
 )
 
 func (c *Client) ListAccounts(userGuid string) ([]*models.Account, error) {
-	return nil, nil
+	if userGuid == "" {
+		return nil, MissingGuid
+	}
+
+	apiEndpointUrl := c.ApiURL + "/users/" + userGuid + "/accounts"
+	response, err := Get(apiEndpointUrl, c.defaultHeaders())
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if err := parseResponseErrors(response.StatusCode); err != nil {
+		return nil, err
+	}
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(response.Body)
+	bufferStr := buffer.String()
+
+	if response.StatusCode == 200 {
+		accountsResponse := &models.AccountsResponse{}
+		json.Unmarshal([]byte(bufferStr), accountsResponse)
+		return accountsResponse.Accounts, nil
+	}
+
+	return nil, makeGenericError(response.StatusCode, bufferStr)
 }
 
-func (c *Client) GetAccounts(userGuid, accountGuid string) (*models.Account, error) {
-	return nil, nil
+func (c *Client) GetAccount(userGuid, accountGuid string) (*models.Account, error) {
+	if userGuid == "" || accountGuid == "" {
+		return nil, MissingGuid
+	}
+
+	apiEndpointUrl := c.ApiURL + "/users/" + userGuid + "/accounts/" + accountGuid
+	response, err := Get(apiEndpointUrl, c.defaultHeaders())
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(response.Body)
+	bufferStr := buffer.String()
+
+	if response.StatusCode == 200 {
+		accountResponse := &models.AccountResponse{}
+		json.Unmarshal([]byte(bufferStr), accountResponse)
+		return accountResponse.Account, nil
+	}
+
+	return nil, makeGenericError(response.StatusCode, bufferStr)
 }
 
 func (c *Client) ListAccountTransations(userGuid, accountGuid string) ([]*models.Transaction, error) {
-	return nil, nil
+	if userGuid == "" || accountGuid == "" {
+		return nil, MissingGuid
+	}
+
+	apiEndpointUrl := c.ApiURL + "/users/" + userGuid + "/accounts/" + accountGuid + "/transactions"
+	response, err := Get(apiEndpointUrl, c.defaultHeaders())
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if err := parseResponseErrors(response.StatusCode); err != nil {
+		return nil, err
+	}
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(response.Body)
+	bufferStr := buffer.String()
+
+	if response.StatusCode == 200 {
+		transactionsResponse := &models.TransactionsResponse{}
+		json.Unmarshal([]byte(bufferStr), transactionsResponse)
+		return transactionsResponse.Transactions, nil
+	}
+
+	return nil, makeGenericError(response.StatusCode, bufferStr)
 }
--- models/transaction.go
+++ models/transaction.go
@@ -1,7 +1,34 @@
 package models
 
 type Transaction struct {
-
+	TransactionTypeName string `json:"transaction_type_name,omitempty"`
+	AccountGuid string `json:"account_guid,omitempty"`
+	Amount float64 `json:"amount,omitempty"`
+	CreatedAt string `json:"created_at,omitempty"`
+	Description string `json:"description,omitempty"`
+	Guid string `json:"guid,omitempty"`
+	HasBeenSplit bool `json:"has_been_split,omitempty"`
+	HasBeenViewed bool `json:"has_been_viewed,omitempty"`
+	IsBillPay bool `json:"is_bill_pay,omitempty"`
+	IsDirectDeposit bool `json:"is_direct_deposit,omitempty"`
+	IsExpense bool `json:"is_expense,omitempty"`
+	IsFee bool `json:"is_fee,omitempty"`
+	IsHidden bool `json:"is_hidden,omitempty"`
+	IsIncome bool `json:"is_income,omitempty"`
+	IsOverdraftFee bool `json:"is_overdraft_fee,omitempty"`
+	IsPayrollAdvance bool `json:"is_payroll_advance,omitempty"`
+	Latitude float64 `json:"latitude,omitempty"`
+	Longitude float64 `json:"longitude,omitempty"`
+	Memo string `json:"memo,omitempty"`
+	MerchantCategoryCode int64 `json:"merchant_category_code,omitempty"`
+	ParentGuid string `json:"parent_guid,omitempty"`
+	PostedAt string `json:"posted_at,omitempty"`
+	TransactionType int64 `json:"transaction_type,omitempty"`
+	TransactedAt string `json:"transacted_at,omitempty"`
+	UpdatedAt string `json:"updated_at,omitempty"`
+	UserGuid string `json:"user_guid,omitempty"`
+	CheckNumber int64 `json:"check_number,omitempty"`
+	FeedCheckNumber int64 `json:"feed_check_number,omitempty"`
 }
 
 type TransactionResponse struct {
