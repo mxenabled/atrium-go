d9a77b44723b91531218dffa4aafd0d7ac37ed96
Brett Mortensen <brett.mortensen@mx.com>
2017-12-01 12:44:55 -0700

    added small and medium urls

--- models/institution.go
+++ models/institution.go
@@ -1,9 +1,11 @@
 package models
 
 type Institution struct {
-	Code string `json"code,omitempty"`
-	Name string `json"name,omitempty"`
-	URL  string `json"url,omitempty"`
+	Code          string `json"code,omitempty"`
+	MediumLogoURL string `json"code,omitempty"`
+	Name          string `json"name,omitempty"`
+	URL           string `json"url,omitempty"`
+	SmallLogoURL  string `json"url,omitempty"`
 }
 
 type InstitutionsResponse struct {
